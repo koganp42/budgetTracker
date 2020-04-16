13b930ad39e429dea1db4251ae2f035f7590828a
Kogan <koganp42@gmail.com>
2020-04-16 18:00:45 -0500

    Removed unnecessary import

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-import dotenv from 'dotenv';
 
 const PORT = process.env.PORT || 3000;
 
