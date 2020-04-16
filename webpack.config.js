051007502711ce604a0424d94c2c6daa9709afa5
Kogan <koganp42@gmail.com>
2020-04-16 15:28:53 -0500

    Configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: "./public/index.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      name: "Budget Tracker",
+      short_name: "Budget Tracker",
+      filename: "manifest.json",
+      inject: false,
+      fingerprints: false,
+      description: "An application that allows you to track your spending online and offline.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [{
+        src: path.resolve(
+          __dirname,
+          "public/icons/icon-512x512.png"
+          ),
+        size: [192, 512],
+      }]
+    })
+  ]
+};
+
+module.exports = config;
