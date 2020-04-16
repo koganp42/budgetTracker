996424a5e83cb9ac3bfa9ebdc67d52926b084cfe
Kogan <koganp42@gmail.com>
2020-04-16 16:37:08 -0500

    Added dotenv

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+import dotenv from 'dotenv';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(compression());
 app.use(express.static("public"));
+app.use(logger("dev"));
+require('dotenv').config();
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
